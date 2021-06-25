1555b9934d2120c1ce1c382b2372c05373a694d8
Paulo <mpmonsale@gmail.com>
2021-06-25 17:37:03 -0400

    Updates to script.js and style.css

--- script.js
+++ script.js
@@ -12,5 +12,12 @@ function createGrid(squaresPerSide) {
     grid.appendChild(square);
     grid.style.gridTemplateRows = "repeat(" + squaresPerSide + ", 1fr)";
     grid.style.gridTemplateColumns = "repeat(" + squaresPerSide + ", 1fr)";
+
+    const allSquares = document.querySelectorAll(".square");
+
+    allSquares.forEach(unit => unit.addEventListener("hover", (e) => {
+      e.target.classList.toggle("square");
+      e.target.classList.toggle("colored-in");
+    }));
   }
 }
